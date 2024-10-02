02fef9c8e0e61b706205522a540faca36cab9d4c
Lucas ferry <lucas.ferry.ecole@gmail.com>
2024-10-02 11:07:40 +0200

    Read datas

--- DHT11/DHT11.ino
+++ DHT11/DHT11.ino
@@ -1,9 +1,34 @@
+#include "DHT.h"
+
+#define DHTPIN 18
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  dht.begin();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(5000);
 
+  float h = dht.readHumidity();
+
+  float t = dht.readTemperature();
+
+  float f = dht.readTemperature(true);
+
+  if(isnan(h) || isnan(t) || isnan(f)){
+    Serial.println("receive failed");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.println(h);
+  Serial.print("Temperature: ");
+  Serial.println(t);
+  Serial.print("Temperature in Farenheit: ");
+  Serial.println(f);
 }
